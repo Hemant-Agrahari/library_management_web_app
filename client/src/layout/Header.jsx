19aa87be546819fef8dfd96a1fee211139853198
Hemant Agarahari <hemantkagrahari@gmail.com>
2025-10-26 22:50:27 +0530

    Implement real-time clock and date display in Header component
    - Added useSelector to retrieve user information from Redux store.
    - Introduced useEffect to set up a real-time clock and date display, updating every second.
    - Enhanced Header component to show current time in 12-hour format with AM/PM and formatted date.

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import settingIcon from "../assets/setting.png";
 import userIcon from "../assets/user.png";
+import { useDispatch, useSelector } from "react-redux";
 
 const Header = () => {
+  const { user } = useSelector((state) => state.auth);
+  const [currentTime, setCurrentTime] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+
+  useEffect(() => {
+const updateDateTime = () => {
+  const dispatch = useDispatch();
+  const now = new Date();
+  const hour = now.getHours() % 12 || 12;
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const ampm = now.getHours() < 12 ? "AM" : "PM";
+  setCurrentTime(`${hour}:${minutes} ${ampm}`);
+  const options={month: 'short', dat: 'numeric', year: 'numeric'};
+  setCurrentDate(now.toLocaleDateString('en-US', options));
+}
+updateDateTime();
+const interval = setInterval(updateDateTime, 1000);
+return () => clearInterval(interval);
+  }, []);
   return <></>;
 };
 
